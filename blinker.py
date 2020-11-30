40b6527b3fd51654d9a53ac4fcce9b0e9067c566
Felix Obenauer <felix.obenauer@qaware.de>
2020-11-30 17:38:05 +0100

    fixing

--- blinker.py
+++ blinker.py
@@ -6,8 +6,14 @@ class Blinker():
     def __init__(self):
         self.occupied = False
         self.blink = Blink1()
+        self.blink.write_pattern_line(100, 'yellow', 1)
+        self.blink.write_pattern_line(100, 'black', 2)
+
+    def set_waiting(self):
+        self.blink.play(1,2,0)
 
     def set_occupied(self, occupied=True):
+
         self.occupied = occupied
         if self.occupied:
             self.blink.fade_to_color(10, 'red')
